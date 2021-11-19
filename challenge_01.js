670cc6cd1b059c24715b7f4750ac81e9485205e0
Niemand21 <tobias_bauer2@web.de>
2021-11-19 09:11:24 +0100

    Solutions added

--- challenge_01.js
+++ challenge_01.js
@@ -9,13 +9,38 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-let numberOne;
+/********  longer Version  *********/
+
+let numberOne; //Variablen deklarieren
 let numberTwo;  
 let summe; 
 
-numberOne = prompt("Zahl einfügen : ");
+numberOne = prompt("Zahl einfügen : "); //Variablen Wert zuweisen
 numberTwo = prompt("Zahl einfügen : ");
 
-summe = (Number(numberOne) + Number(numberTwo));
+summe = (Number(numberOne) + Number(numberTwo)); // Berechnung & Typorisierung
 
-console.log("Das Ergebnis ist: " + summe);
+console.log("Das Ergebnis ist: " + summe); // Ausgabe
+
+/********  shorter Version  *********/
+
+let numberOne = prompt("Zahl einfügen : "); //Deklaration & Wertzuweisung
+let numberTwo = prompt("Zahl einfügen : ");  
+let summe = (Number(numberOne) + Number(numberTwo)); //Berechnung & Typorisierung
+console.log("Das Ergebnis ist: " + summe); // Ausgabe
+
+/********  Other short Version  *********/
+
+let numberOne = parseInt(prompt("Zahl 1 eingeben: ")); // Variable deklarieren, Wert bestimmen & Typorisierung
+let numberTwo = parseInt(prompt("Zahl 2 eingeben: "));
+let summe = numberOne + numberTwo;                     // Berechnung 
+console.log("Die Summe ist: " + summe);                // Ausgabe
+
+/********  Other long Version  *********/
+
+let zahl1 = prompt("gib die erste Zahl ein");  //Deklaration & Wertzuweisung
+let zahl2 = prompt("gib die zweite Zahl ein");
+
+let summe = parseInt(zahl1) + parseInt(zahl2);  //Berechnung & Typorisierung
+
+console.log("Die Summe der Zahlen ist: " +summe);  // Ausgabe
