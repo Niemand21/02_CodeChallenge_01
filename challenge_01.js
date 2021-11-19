8bd906efd754748f577311f91a575208d84c275d
Niemand21 <tobias_bauer2@web.de>
2021-11-19 08:09:07 +0100

    test_comit

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,14 @@ Berechnen Sie die Summe der beiden Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+let numberOne;
+let numberTwo;  
+let summe; 
+
+numberOne = prompt("Zahl einfügen : ");
+numberTwo = prompt("Zahl einfügen : ");
+
+summe = (Number(numberOne) + Number(numberTwo));
+
+console.log("Das Ergebnis ist: " + summe);
